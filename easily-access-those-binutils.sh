6651862fe23a8febbd130b3b7efa02d85d1fc189
Matt Pagan <matt@pagan.io>
2014-07-26 17:04:04 -0500

    Cross-shell compatibility matters.

--- easily-access-those-binutils.sh
+++ easily-access-those-binutils.sh
@@ -2,7 +2,7 @@ targetcpu=armv5te
 targetdir=/usr/local/bin
 origdir=/opt/eldk-5.5/$targetcpu/sysroots/i686-eldk-linux/usr/bin/$targetcpu-linux-gnueabi;
 
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
   echo "Error: Not root user."
   exit 1
 fi
