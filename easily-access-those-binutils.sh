0ecb8159a1f56f53ae4a61fe23ed9d0a85e9a6ba
Matt Pagan <matt@pagan.io>
2014-07-26 14:07:06 -0500

    Check for root

--- easily-access-those-binutils.sh
+++ easily-access-those-binutils.sh
@@ -2,6 +2,11 @@ targetcpu=armv5te
 targetdir=/usr/local/bin
 origdir=/opt/eldk-5.5/$targetcpu/sysroots/i686-eldk-linux/usr/bin/$targetcpu-linux-gnueabi;
 
+if (( $EUID !0 )); then
+  echo "Error: not root user"
+  exit 1
+fi
+
 cd $origdir
 
 for i in $(ls arm-linux*); do ln -s $PWD/$i $targetdir/$(echo $i | sed s/arm-linux-gnueabi/arm-linux/ ); done
